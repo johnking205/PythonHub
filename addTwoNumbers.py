37d70a5c1b7a93582e8eab9c068f6b8cbe054f4d
John <johnking205@gmail.com>
2017-10-27 21:09:29 -0400

    Working for some test cases, but not all. Solution is becoming too complex. Sorely in need of refactorization or starting from scratch.

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -9,27 +9,60 @@ class Solution(object):
         """
         :type l1: ListNode
         :type l2: ListNode
-        :rtype: ListNode
+        :rtype: array of ListNode.val
         """
         currentNode1 = l1
         currentNode2 = l2
         carry = 0
-        
-        while(currentNode1.next != None):
+        sumNodes = []
+
+        while (currentNode1 != None) and (currentNode2 != None):
             #logic for adding linked lists together
+            currentSumNode = ListNode(0)
+            currentSumNode.val = (currentNode1.val + currentNode2.val + carry) % 10
+            sumNodes.append(currentSumNode)
+            if(currentNode1.val + currentNode2.val + carry) >= 10:
+                carry = 1
+            else:
+                carry = 0
+            
+            currentNode1 = currentNode1.next
+            currentNode2 = currentNode2.next
+
+#        if carry == 1:
+#           sumNodes.append(ListNode(1))
+        if currentNode1 != None:
+            sumNodes.append(currentNode1)
+            sumNodes[-1].val = (sumNodes[-1].val + carry) % 10
+            if(sumNodes[-1].val + carry) == 1:
+                sumNodes.append(ListNode(1))
+        elif currentNode2 != None:
+            sumNodes.append(currentNode2)
+            sumNodes[-1].val = (sumNodes[-1].val + carry) % 10
+            if(sumNodes[-1].val + carry) == 1:
+                sumNodes.append(ListNode(1))
+        elif carry == 1:
+            sumNodes.append(ListNode(1))
+
+        nodeVals = []
+        for i in sumNodes:
+            nodeVals.append(i.val)
+        
+        return nodeVals
+
 
 x = Solution()
 
-l11 = ListNode(2)
-l12 = ListNode(4)
-l13 = ListNode(3)
-l11.next = l12
-l12.next = l13
+l11 = ListNode(0)
+#l12 = ListNode(8)
+#l13 = ListNode(3)
+#l11.next = l12
+#l12.next = l13
 
-l21 = ListNode(5)
-l22 = ListNode(6)
-l23 = ListNode(4)
-l21.next = l22
-l22.next = l23
+l21 = ListNode(0)
+#l22 = ListNode(9)
+#l23 = ListNode(4)
+#l21.next = l22
+#l22.next = l23
 
-x.addTwoNumbers(l11,l21)
+print(x.addTwoNumbers(l11,l21))
